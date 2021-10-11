8e44d0a45c152aedabe1592d96a73ff1b0411d1a
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-11 21:53:50 +0530

    Removed the word from phrase

--- src/com/company/WordCounter.java
+++ src/com/company/WordCounter.java
@@ -4,6 +4,20 @@ import java.util.*;
 
 public class WordCounter {
 
+    static void remove(String str, String removeWord) {
+        String msg[] = str.split(" ");
+        String new_str = " ";
+        for (String words : msg) {
+            if (!words.equals(removeWord)) {
+                new_str += words + " ";
+            }
+        }
+        System.out.println();
+        System.out.println("Removed the word -> avoidable");
+        System.out.println(new_str);
+    }
+
+
     public static void main(String[] args) {
 
         String sentence = "To be or not to be";
@@ -18,16 +32,22 @@ public class WordCounter {
         }
         System.out.println(words);
 
-    String sentence2 = "Paranoids are not paranoid because they are paranoid but" +
-            "because they keep putting themselves deliberately into paranoid avoidable situations";
-    String b[] = sentence2.toLowerCase().split(" ");
-    Map<String,Integer> words2 = new HashMap<>();
-    for (String word : b) {
-        if (words2.containsKey(word)) {
-            words2.put(word,1 + words2.get(word));
-        } else {
-            words2.put(word,1);
+        String sentence2 = "Paranoids are not paranoid because they are paranoid but" +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
+        String b[] = sentence2.toLowerCase().split(" ");
+        Map<String, Integer> words2 = new HashMap<>();
+        for (String word : b) {
+            if (words2.containsKey(word)) {
+                words2.put(word, 1 + words2.get(word));
+            } else {
+                words2.put(word, 1);
+            }
         }
-    } System.out.println(words2);
+        System.out.println(words2);
+
+        String str = "Paranoids are not paranoid because they are paranoid but" +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
+        String removeWord = "avoidable";
+        remove(str, removeWord);
     }
 }
